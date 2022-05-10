63c102c08090b3fb0ba258425c2d8efb151f5639
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <jankee112@freenet.de>
2022-05-10 15:50:31 +0200

    connect MONGODB

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
 const app = express();
 
 app.listen(3000, function () {
   console.log("listening on 3000");
 });
 
+MongoClient.connect(
+  "mongodb+srv://dbUser:Sdafskizpae4QQhB@cluster0.gatr2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    if (err) return console.error(err);
+    console.log("Connected to Database");
+  }
+);
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
+
+app.post("/quotes", (req, res) => {
+  console.log(req.body);
+});
+
